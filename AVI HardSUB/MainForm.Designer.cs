d62c2ee998f5e903677a5d6db4b7568f10ca0b81
kevinlimantoro <vinlimantoro@gmail.com>
2014-10-30 16:59:55 +0800

    AVI-HardSUB
    Avi Hard Sub, is a video-hardsubber application running on Windows. This
    application made using visual c# and of course AV Library. The input of
    this application is video and text file, the format for text file is
    similar to SRT. User can edit the SRT and hardsub the video using this
    application.

---
+++ AVI HardSUB/MainForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace AVI_HardSUB
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.BrowseFile = new System.Windows.Forms.Button();
+            this.txtAviFile = new System.Windows.Forms.TextBox();
+            this.Play = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mediaplayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lyric = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.HardsubBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaplayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BrowseFile
+            // 
+            this.BrowseFile.Location = new System.Drawing.Point(436, 3);
+            this.BrowseFile.Name = "BrowseFile";
+            this.BrowseFile.Size = new System.Drawing.Size(75, 23);
+            this.BrowseFile.TabIndex = 0;
+            this.BrowseFile.Text = "Browse";
+            this.BrowseFile.UseVisualStyleBackColor = true;
+            this.BrowseFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAviFile
+            // 
+            this.txtAviFile.Enabled = false;
+            this.txtAviFile.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtAviFile.Location = new System.Drawing.Point(15, 6);
+            this.txtAviFile.Name = "txtAviFile";
+            this.txtAviFile.Size = new System.Drawing.Size(415, 20);
+            this.txtAviFile.TabIndex = 1;
+            this.txtAviFile.Text = "Browse Your File";
+            this.txtAviFile.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(133, 302);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 4;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // mediaplayer
+            // 
+            this.mediaplayer.Enabled = true;
+            this.mediaplayer.Location = new System.Drawing.Point(15, 41);
+            this.mediaplayer.Name = "mediaplayer";
+            this.mediaplayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaplayer.OcxState")));
+            this.mediaplayer.Size = new System.Drawing.Size(496, 313);
+            this.mediaplayer.TabIndex = 7;
+            this.mediaplayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.mediaplayer_PlayStateChange);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 26);
+            this.label2.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Time,
+            this.Lyric});
+            this.dataGridView1.Location = new System.Drawing.Point(529, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(456, 313);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            // 
+            // Lyric
+            // 
+            this.Lyric.HeaderText = "Lyric";
+            this.Lyric.Name = "Lyric";
+            this.Lyric.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Lyric.Width = 600;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Get Time";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(529, 381);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Location = new System.Drawing.Point(529, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Insert at                   Lyric:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(624, 381);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(910, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(526, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Just Double Click To edit";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(745, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Save Lyric!!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // HardsubBtn
+            // 
+            this.HardsubBtn.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HardsubBtn.Location = new System.Drawing.Point(235, 368);
+            this.HardsubBtn.Name = "HardsubBtn";
+            this.HardsubBtn.Size = new System.Drawing.Size(195, 33);
+            this.HardsubBtn.TabIndex = 19;
+            this.HardsubBtn.Text = "HardSub Now !!";
+            this.HardsubBtn.UseVisualStyleBackColor = true;
+            this.HardsubBtn.Click += new System.EventHandler(this.HardsubBtn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(888, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Who Make This?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(997, 403);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.HardsubBtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mediaplayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.txtAviFile);
+            this.Controls.Add(this.BrowseFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Let\'s Karaoke";
+            ((System.ComponentModel.ISupportInitialize)(this.mediaplayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseFile;
+        private System.Windows.Forms.TextBox txtAviFile;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer mediaplayer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lyric;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button HardsubBtn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ AVI HardSUB/MainForm.cs
@@ -0,0 +1,358 @@
+﻿#region Using directives
+using AviFile;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+#endregion
+
+
+namespace AVI_HardSUB
+{
+    public partial class MainForm : Form
+    {
+        private delegate void SimpleDelegate();
+
+        private AviPlayer player;
+        private EditableVideoStream editableStream;
+        string path;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private String GetFileName(String filter)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = filter;
+            dlg.RestoreDirectory = true;
+            if (txtAviFile.Text.Length > 0)
+            {
+                dlg.InitialDirectory = txtAviFile.Text.Substring(0, txtAviFile.Text.LastIndexOf("\\") + 1);
+            }
+            if (dlg.ShowDialog(this) == DialogResult.OK)
+            {
+                return dlg.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void Hardsub()
+        {
+            string minstr, secstr, imgtime; int row,i,sec,min;bool found=false;
+            if (System.IO.File.Exists(txtAviFile.Text))
+            {
+                
+                    path = txtAviFile.Text.Substring(0, txtAviFile.Text.Length - 4);
+                    path = path + "3.avi";
+                    
+                    AviManager newFile = new AviManager(path,false);
+                    try{
+                    AviManager existvideo = new AviManager(txtAviFile.Text, true);
+                    VideoStream videoStream = existvideo.GetVideoStream();
+
+                    videoStream.GetFrameOpen();
+                    Bitmap bmp = videoStream.GetBitmap(0);
+
+                    VideoStream newStream = newFile.AddVideoStream(true, videoStream.FrameRate, bmp);
+                        int x, y;
+                        x = videoStream.Width / 100 * 15;
+                        y = videoStream.Height / 100 * 95;
+                        string lirik = ""; string nxtime; bool OkChange;
+                        int Pstring,IndeksSubtitle,indeks,nextsubframe,colorchange,count;
+                        indeks = 0;
+                        IndeksSubtitle = 0;
+                        count = 0;
+                        colorchange = 0;
+                        OkChange = false;
+
+                    for (int n = 1;
+                                 n <= videoStream.CountFrames; n++)
+                    {
+                        sec = n / (int)videoStream.FrameRate;
+                        min = sec / 60;
+                        if (sec >= 60)
+                        {
+                            sec = sec % 60;
+                            minstr = min.ToString();
+                            secstr = sec.ToString();
+                        }
+                        else
+                        {
+                            minstr = min.ToString();
+                            secstr = sec.ToString();
+                        }
+
+                        if (minstr.Length < 2)
+                        {
+                            minstr = "0" + minstr;
+                        }
+
+                        if (secstr.Length < 2)
+                        {
+                            secstr = "0" + secstr;
+                        }
+
+                        imgtime = minstr + ":" + secstr;
+
+                        for (i = 0; i < dataGridView1.RowCount - 1; i++)
+                        {
+                            if (imgtime == dataGridView1.Rows[i].Cells[0].Value.ToString().Substring(1, 5)&&indeks!=i)
+                            {
+                                lirik = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                                Pstring = lirik.Length;
+                                IndeksSubtitle = 0;
+                                indeks = i;
+                                colorchange = 15;
+                                nxtime = dataGridView1.Rows[i+1].Cells[0].Value.ToString().Substring(1,5);
+                                nextsubframe = Convert.ToInt32(nxtime.Substring(0, 2)) * 60 * (int)videoStream.FrameRate;
+                                nextsubframe = nextsubframe + (Convert.ToInt32(nxtime.Substring(3, 2)) * (int)videoStream.FrameRate);
+                                if (Pstring > 0)
+                                { colorchange = (nextsubframe - n) / Pstring; OkChange = true; count = 0; }
+                                else if(Pstring==0)
+                                { OkChange = false; count = 0; }
+                                found = true;
+                            }
+                        }
+                        if (found == true)
+                        {
+                            Bitmap bmp2;
+                            bmp2 = videoStream.GetBitmap(n);
+                            Graphics g;
+                            g = Graphics.FromImage(bmp2);
+                            g.DrawString(lirik, new Font("Tahoma", 18), Brushes.NavajoWhite, new PointF(x, y));
+                            g.DrawString(lirik.Substring(0, IndeksSubtitle), new Font("Tahoma", 18), Brushes.DeepSkyBlue, new PointF(x, y));
+                            //cari supaya isa bold, ato color chooser;
+                            newStream.AddFrame(bmp2);
+                            bmp2.Dispose();
+                            g.Dispose();
+                            count++;
+
+                        }
+                        if(found==false)
+                        {
+                            Bitmap bmp2;
+                            bmp2 = videoStream.GetBitmap(n);
+                            newStream.AddFrame(bmp2);
+                            bmp2.Dispose();
+                        }
+                        
+                        if (count == colorchange&&found==true&&OkChange==true)
+                        {
+                            if (IndeksSubtitle < lirik.Length)
+                            {
+                                IndeksSubtitle++;
+                            }
+                                count = 0;
+                        }
+
+                        
+                        
+
+                    }
+                        
+                    
+                  
+                    videoStream.GetFrameClose();
+                    existvideo.Close();
+                    
+
+                }
+                catch (Exception ex)
+                {   
+                    newFile.Close();
+                    //throw ex;
+                }
+                 string path3 = path.Substring(0, path.Length - 3);
+                 path3 = path3 + "wav";
+                AviManager newFile2 = new AviManager(path, true);
+                newFile2.AddAudioStream(path3, 0);
+                newFile2.Close();
+
+            }
+        }
+
+        private void ExtractSound()
+        {
+            
+            string path = txtAviFile.Text;
+            AviManager avi = new AviManager(path, true);
+            AudioStream temp = avi.GetWaveStream();
+            path = path.Substring(0, path.Length - 4);
+            path = path + "3.wav";
+            temp.ExportStream(path);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String filename=GetFileName("Videos (*.mkv)|*.avi;*.mpe;*.mkv");
+            if (filename != null) {
+                txtAviFile.Text = filename;
+                
+                
+
+                mediaplayer.URL = txtAviFile.Text; 
+               
+                path=txtAviFile.Text.Substring(0, txtAviFile.Text.Length - 3);
+                path = path + "lrc";
+                if (System.IO.File.Exists(path))
+                {
+                    TextReader SubLrc = new StreamReader(path);
+                    string ts;
+                    ts = "a";
+                    while (ts!=null)
+                    {     
+                        ts = SubLrc.ReadLine();
+                        if(ts!=null)
+                        {
+                            if (ts.Length>8&&ts.Substring(9, 1) == "]")
+                            {
+                                dataGridView1.Rows.Add(ts.Substring(0, 10), ts.Substring(10));
+                            }
+                        } timer1.Enabled = true;
+                    } 
+                    //mediaplayer.closedCaption.SAMIFileName = path;
+                }
+                else 
+                {
+                    dataGridView1.Rows.Add("No Subtitle Found", "-") ;
+                }
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string a,b;int i;
+            a = mediaplayer.Ctlcontrols.currentPosition.ToString();
+            b = mediaplayer.Ctlcontrols.currentPositionString;
+            if (a != "0")
+            { a = a.Substring(a.IndexOf('.'), 3); }
+            a = b+a ;
+            button1.Text = a;
+            for (i = 0; i < dataGridView1.RowCount-1; i++)
+            {
+                if (a.Length>=8&&dataGridView1.Rows[i].Cells[0].Value.ToString().Length>=8)
+                {
+                    if (a.Substring(0, 7) == dataGridView1.Rows[i].Cells[0].Value.ToString().Substring(1, 7))
+                    {
+                        label2.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                        dataGridView1.ClearSelection();
+                        dataGridView1.Rows[i].Selected = true;
+                        dataGridView1.CurrentCell = dataGridView1[0, i];
+                    }
+                }
+            }
+            
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            textBox1.Text = "[" + button1.Text + "]";
+            mediaplayer.Ctlcontrols.pause();
+
+                /*string a=dataGridView1.Rows[0].Cells[1].Value.ToString();
+                dataGridView1.ClearSelection();
+                dataGridView1.Rows[20].Cells[1].Selected = true;
+                dataGridView1.CurrentCell = dataGridView1[0, 20];
+                MessageBox.Show(a);*/
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text);
+            dataGridView1.Sort(dataGridView1.Columns[0],System.ComponentModel.ListSortDirection.Ascending);
+        }
+
+        private void mediaplayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            switch (e.newState)
+            { 
+                case 1: //Stopped
+                    timer1.Enabled = false;
+                    mediaplayer.close();
+                    HardsubBtn.Enabled = true;
+                    break;
+
+                case 3:
+                    label2.Text = "";
+                    timer1.Enabled = true;
+                    HardsubBtn.Enabled = false;
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i;
+            path = txtAviFile.Text.Substring(0, txtAviFile.Text.Length - 3);
+            path = path + "lrc";
+            StreamWriter wr = new StreamWriter(path);
+            for (i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                wr.WriteLine(dataGridView1.Rows[i].Cells[0].Value.ToString() + dataGridView1.Rows[i].Cells[1].Value.ToString());
+            }
+            wr.Flush();
+            wr.Close();
+        }
+
+        private void HardsubBtn_Click(object sender, EventArgs e)
+        {
+            
+            ExtractSound();
+            Hardsub();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form a=new AboutBox1();
+            a.Show();
+
+            //MessageBox.Show("Kevin Darmawan L.\t26409022\nAndreanus Agung\t26409023\nPaul Agustinus\t26409034\nFujianto\t\t26409038\nNikolas Wijaya K.\t26409045","Kelompok 18");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AviManager a = new AviManager(txtAviFile.Text, true);
+            VideoStream b = a.GetVideoStream();
+            b.GetFrameOpen();
+            Bitmap c = b.GetBitmap(100);
+            StringFormat strfrmt=new StringFormat();
+            strfrmt.Alignment=StringAlignment.Center;
+            Graphics ga = Graphics.FromImage(c);
+            ga.DrawString("hahaha", new Font("Tahoma", 20), Brushes.Red, new PointF(400,0),strfrmt);
+            //c=new Bitmap(640,360,ga);
+            string path = txtAviFile.Text.Substring(0, txtAviFile.Text.Length - 3);
+            path=path+"bmp";
+            c.Save(path);
+            b.GetFrameClose();
+            b.Close();
+            a.Close();
+            /*double c = (double)b.CountFrames / b.FrameRate;
+            c=Math.Round(c, 0);
+            MessageBox.Show(c.ToString());*/
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            string asa = "05"; int ada;
+            AviManager a = new AviManager(txtAviFile.Text, true);
+            VideoStream b = a.GetVideoStream();
+            ada = Convert.ToInt32(asa) + (int)b.FrameRate;
+            asa = ada.ToString();
+            MessageBox.Show(asa.Substring(1,1));
+            
+        }
+
+    }
+}
